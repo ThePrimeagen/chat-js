f37e1d0db2b0cdabd7015b0c60c7529eb2c6d61e
mpaulson <the.primeagen@gmail.com>
2023-09-12 14:07:32 -0600

    fix: back to using ws

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import ws from "ws";
 import cli from "command-line-args";
 import { Chat } from "./set-chat";
 import { createRoom } from "./room/set-room";
-import uWS from "uWebSockets.js";
 
 const args = cli([{
     name: "port",
@@ -10,24 +10,10 @@ const args = cli([{
     defaultValue: 42067,
 }]);
 
+const wss = new ws.Server({ port: args.port });
 const chat = new Chat(createRoom);
 
-uWS.App()
-    .ws("/*", {
-        open(ws) {
-        },
-
-        message(ws, message, isBinary) {
-        },
-
-        close(ws, code, message) {
-        }
-    }).listen(args.port, (listenSocket) => {
-        if (listenSocket) {
-            console.log(`Listening to port ${args.port}`);
-        } else {
-            console.log("failed to listen");
-            process.exit(1);
-        }
-    });
+wss.on("connection", (ws) => {
+    chat.add(ws);
+});
 
